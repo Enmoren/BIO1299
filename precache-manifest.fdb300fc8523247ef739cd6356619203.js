d7b6610c08e31cf0ac93ac3a962d88c8596d2921
Enmoren <enmo.ren@students.olin.edu>
2020-12-06 07:00:45 +0800

    deploy

---
+++ precache-manifest.fdb300fc8523247ef739cd6356619203.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491a8b505cfd563f673a",
+    "url": "/BIO1299/css/app.c00dbbe4.css"
+  },
+  {
+    "revision": "e3ee7dbcbc43065f50b0",
+    "url": "/BIO1299/css/chunk-vendors.2554ad22.css"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "/BIO1299/fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "/BIO1299/fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "/BIO1299/fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "/BIO1299/fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "a7acc6b0f7f7a38e6319ca4bfdc79d23",
+    "url": "/BIO1299/index.html"
+  },
+  {
+    "revision": "491a8b505cfd563f673a",
+    "url": "/BIO1299/js/app.8f500090.js"
+  },
+  {
+    "revision": "e3ee7dbcbc43065f50b0",
+    "url": "/BIO1299/js/chunk-vendors.1fe250d8.js"
+  },
+  {
+    "revision": "c6f74ae1bf2d86e38882c250b52aaa5d",
+    "url": "/BIO1299/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BIO1299/robots.txt"
+  },
+  {
+    "revision": "e176eeac7a6d9a5014c0fe5739c3a783",
+    "url": "/BIO1299/static/Chart/css/index.css"
+  },
+  {
+    "revision": "1accc45ce4b4d2a25d40fd0e9620762a",
+    "url": "/BIO1299/static/Chart/index.html"
+  },
+  {
+    "revision": "5d98adc2c09b812c26cb7d74dd50152e",
+    "url": "/BIO1299/static/Chart/js/4add81f750.js"
+  },
+  {
+    "revision": "c6f94c3dea5d7fdc402faa64986f049d",
+    "url": "/BIO1299/static/Chart/js/data.js"
+  },
+  {
+    "revision": "f63f83710fb4524515684af4867ab73b",
+    "url": "/BIO1299/static/Chart/js/index.js"
+  },
+  {
+    "revision": "43e0a7d0496e085696a01339bceef3df",
+    "url": "/BIO1299/static/Chart/js/react-dom.development.js"
+  },
+  {
+    "revision": "bff81f643a904be8a3d144f7a8142c24",
+    "url": "/BIO1299/static/Chart/js/react.development.js"
+  },
+  {
+    "revision": "b241e12d05ff3947fb3cd7fc48b4e639",
+    "url": "/BIO1299/static/Chart/js/translate.js"
+  },
+  {
+    "revision": "0b8235b54b9a6feae537171ed2387f8d",
+    "url": "/BIO1299/static/Chart/js/utils.js"
+  },
+  {
+    "revision": "00059c935441d6f79bbd8a18c283cf86",
+    "url": "/BIO1299/static/China/README.md"
+  },
+  {
+    "revision": "8f758d23203d1169f2cfff868f04806a",
+    "url": "/BIO1299/static/China/_config.yml"
+  },
+  {
+    "revision": "079d764df981f1c150665ea7986747ff",
+    "url": "/BIO1299/static/China/getOnsInfo.json"
+  },
+  {
+    "revision": "59ef584b7bd5fabf1b4caaafc4d564bf",
+    "url": "/BIO1299/static/China/index.html"
+  },
+  {
+    "revision": "71a4504d2779be2f3854017ce52618cf",
+    "url": "/BIO1299/static/China/js/echarts.min.js"
+  },
+  {
+    "revision": "2028980be09d280e435a23988747a60d",
+    "url": "/BIO1299/static/China/js/map/china.js"
+  },
+  {
+    "revision": "46ef46f7e69d33a226ccfa1ab2b71bdd",
+    "url": "/BIO1299/static/China/js/map/province/anhui.js"
+  },
+  {
+    "revision": "f8d7ab99a6f10f59657e8b8576ac8e8f",
+    "url": "/BIO1299/static/China/js/map/province/aomen.js"
+  },
+  {
+    "revision": "95212bfdbce4bb9fb057120512bb760d",
+    "url": "/BIO1299/static/China/js/map/province/beijing.js"
+  },
+  {
+    "revision": "812b8ab79433a73966a54bbd2375252d",
+    "url": "/BIO1299/static/China/js/map/province/chongqing.js"
+  },
+  {
+    "revision": "e288abf7937b28dbbaca03dd137932cc",
+    "url": "/BIO1299/static/China/js/map/province/fujian.js"
+  },
+  {
+    "revision": "f57c460295905e70ee6a660996822288",
+    "url": "/BIO1299/static/China/js/map/province/gansu.js"
+  },
+  {
+    "revision": "542c331c970b24a09b5c5aba73eefc93",
+    "url": "/BIO1299/static/China/js/map/province/guangdong.js"
+  },
+  {
+    "revision": "ca07ed68d4bd2ed3f81d74017f2ab274",
+    "url": "/BIO1299/static/China/js/map/province/guangxi.js"
+  },
+  {
+    "revision": "785856b1b32349e1e13a47bfd61a1646",
+    "url": "/BIO1299/static/China/js/map/province/guizhou.js"
+  },
+  {
+    "revision": "0ca3052c2b8ed22a18c325beb309deec",
+    "url": "/BIO1299/static/China/js/map/province/hainan.js"
+  },
+  {
+    "revision": "bed65dbff7178542265ad630209fae48",
+    "url": "/BIO1299/static/China/js/map/province/hebei.js"
+  },
+  {
+    "revision": "b9fc74245aba9bfe1b5fc1e9de7ed4e2",
+    "url": "/BIO1299/static/China/js/map/province/heilongjiang.js"
+  },
+  {
+    "revision": "a460074c4aeff1fccfb1489e839798c0",
+    "url": "/BIO1299/static/China/js/map/province/henan.js"
+  },
+  {
+    "revision": "7079255342592890a7e87d988492efd6",
+    "url": "/BIO1299/static/China/js/map/province/hubei.js"
+  },
+  {
+    "revision": "de251dd088ee73d233aad8fb17a14e20",
+    "url": "/BIO1299/static/China/js/map/province/hunan.js"
+  },
+  {
+    "revision": "7f791cbaab13398f51a70007f42cbd91",
+    "url": "/BIO1299/static/China/js/map/province/jiangsu.js"
+  },
+  {
+    "revision": "562216bc80c2b5bf9aa201f17b3fe55e",
+    "url": "/BIO1299/static/China/js/map/province/jiangxi.js"
+  },
+  {
+    "revision": "ad930ab317182d42074307c8d8fbd24f",
+    "url": "/BIO1299/static/China/js/map/province/jilin.js"
+  },
+  {
+    "revision": "d1cef4b37ef7a3b78f83b6d3cdd9c9c0",
+    "url": "/BIO1299/static/China/js/map/province/liaoning.js"
+  },
+  {
+    "revision": "ebd2711a4203c7366d1a7daba842c251",
+    "url": "/BIO1299/static/China/js/map/province/neimenggu.js"
+  },
+  {
+    "revision": "7d7b0015c845cdb7189db5d0ff619520",
+    "url": "/BIO1299/static/China/js/map/province/ningxia.js"
+  },
+  {
+    "revision": "8465b53f0623ce0684fe087921419461",
+    "url": "/BIO1299/static/China/js/map/province/qinghai.js"
+  },
+  {
+    "revision": "db8f8c73e8cc8a22100c16cb73184bf9",
+    "url": "/BIO1299/static/China/js/map/province/shandong.js"
+  },
+  {
+    "revision": "9557109440226aa62dc72e26b606c600",
+    "url": "/BIO1299/static/China/js/map/province/shanghai.js"
+  },
+  {
+    "revision": "821d850b83c78db16819b20d7d8d272c",
+    "url": "/BIO1299/static/China/js/map/province/shanxi.js"
+  },
+  {
+    "revision": "51d941cb571216183737428a0a938d25",
+    "url": "/BIO1299/static/China/js/map/province/shanxi1.js"
+  },
+  {
+    "revision": "29df185ca829675d0e08780169ff6265",
+    "url": "/BIO1299/static/China/js/map/province/sichuan.js"
+  },
+  {
+    "revision": "1cff78aa456d6e192c84029f58bc2965",
+    "url": "/BIO1299/static/China/js/map/province/taiwan.js"
+  },
+  {
+    "revision": "87474bdeec6fa7e7647b6cb80cd3edc5",
+    "url": "/BIO1299/static/China/js/map/province/tianjin.js"
+  },
+  {
+    "revision": "63231a50394e1ac7c2aead892924564e",
+    "url": "/BIO1299/static/China/js/map/province/xianggang.js"
+  },
+  {
+    "revision": "96973c8776c0ff89456d4cf111842723",
+    "url": "/BIO1299/static/China/js/map/province/xinjiang.js"
+  },
+  {
+    "revision": "f41c6fadb6fe27b08d00352a0c657f42",
+    "url": "/BIO1299/static/China/js/map/province/xizang.js"
+  },
+  {
+    "revision": "d4f1a0d8af7d36e6d22cf957e65c1224",
+    "url": "/BIO1299/static/China/js/map/province/yunnan.js"
+  },
+  {
+    "revision": "8631a5318db56cf1d06bce4f498e62dc",
+    "url": "/BIO1299/static/China/js/map/province/zhejiang.js"
+  },
+  {
+    "revision": "e63531350b726384f625ead641f5ad66",
+    "url": "/BIO1299/static/China/libs/axios.min.js"
+  },
+  {
+    "revision": "a365baf8434ecd2c6f0763223a73a7eb",
+    "url": "/BIO1299/static/China/province.json"
+  },
+  {
+    "revision": "e8797bebd49bff7eeff4cbcbed403b71",
+    "url": "/BIO1299/static/Ranking/color_ranges.js"
+  },
+  {
+    "revision": "6dc0e8ceff34fc42d60ee0def099ca72",
+    "url": "/BIO1299/static/Ranking/config.js"
+  },
+  {
+    "revision": "e088ec1265344580c0a91329eea3cb6d",
+    "url": "/BIO1299/static/Ranking/imgs.js"
+  },
+  {
+    "revision": "0182bec1dfe175847a99e7dd73354ff6",
+    "url": "/BIO1299/static/Ranking/index.html"
+  },
+  {
+    "revision": "69c949bd82daa3f46c31124772cdfda0",
+    "url": "/BIO1299/static/Ranking/nCov_china_0319.csv"
+  },
+  {
+    "revision": "0292fcf5ce246d7f5c49a147d5f31eb0",
+    "url": "/BIO1299/static/Ranking/nCov_world_0516.csv"
+  },
+  {
+    "revision": "714e84cd5b5321f8afc579e4ef96078d",
+    "url": "/BIO1299/static/Ranking/nCov_wuhan_0319.csv"
+  },
+  {
+    "revision": "efd034f1877405aa24dd7efb7735fcfe",
+    "url": "/BIO1299/static/Ranking/stylesheet.css"
+  },
+  {
+    "revision": "aa7587ef52fb48ebbe054229592e7c00",
+    "url": "/BIO1299/static/Ranking/university.csv"
+  },
+  {
+    "revision": "2b898163841edbfccd344792fad2e32f",
+    "url": "/BIO1299/static/Ranking/university_ranking.csv"
+  },
+  {
+    "revision": "246aabd5663d5429f9bdc9fe95b32a9d",
+    "url": "/BIO1299/static/Ranking/visual.js"
+  }
+]);
